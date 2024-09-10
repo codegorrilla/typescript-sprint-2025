431706f509dd289a09b7a74cb668c3c59322342d
codegorrilla <code.gorrilla@gmail.com>
2024-09-10 11:27:05 +0530

    comming practices done at home on 09/10/24

---
+++ basics.ts
@@ -0,0 +1,4 @@
+console.log('Hello');
+console.log('Goodbye');
+console.log(Math.round(7.32));
+
---
+++ functions.js
@@ -0,0 +1,28 @@
+//all about function annotations in typescript
+function mySquare(num) {
+    return num * num;
+}
+function myGreet(person) {
+    return "Hello there! ".concat(person);
+}
+//with a default value
+function myNameIs(name) {
+    if (name === void 0) { name = 'Sanjib'; }
+    return "My name is ".concat(name);
+}
+//arrow function
+var doSomethingNow = function (person, age, isFunny) {
+    return "".concat(person, " is ").concat(age, " years old and is ").concat(isFunny ? false && 'not funny' : true && 'very funny');
+};
+mySquare(4);
+myGreet('Dennis');
+console.log(doSomethingNow('Chickenface', 70, true));
+var myContent = document.createElement('div');
+document.documentElement.appendChild(myContent).innerText = myNameIs();
+//return type annotation
+var addition = function (x, y) {
+    return x + y;
+};
+console.log(addition(3, 5));
+//avoid the clash between variable names used here with the typescript global typings
+//export{all}
