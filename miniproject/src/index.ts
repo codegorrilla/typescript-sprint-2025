5d0f210d631a47c1f0ad2b0f9e95fa10423692ac
Sanjib Adhya <sanjib@zacksresearch.in>
2024-12-24 08:15:27 -0600

    added miniproject that contains examples of typeassertions, non-null assertion operator and optional chaining operator

---
+++ miniproject/src/index.ts
@@ -0,0 +1,27 @@
+const btn = document.getElementById('btn')! as HTMLButtonElement; //non-null assertion operator -> generally we dont use it in TS though it's a TS operator to assume the element is not a null
+
+const input = document.getElementById('todoinput')! as HTMLInputElement;
+
+// btn?.addEventListener('click', () => { //? is optional chaining operator
+//     console.log(btn);
+// });
+
+// const hello: string = "hello";
+
+// "hello".replaceAll("l", "!");
+
+//type assertions
+// let word: unknown = "hello";
+
+// const numChars = (word as string).length;
+
+// console.log(`${numChars} characters`);
+
+//real world example for type assertion
+btn.addEventListener('click', () => {
+    alert(input.value);
+    input.value = '';
+})
+
+//alternate syntax
+//(<HTMLInputElement>input).value => don't use them in react
