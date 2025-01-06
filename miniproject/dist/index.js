fea34caf973b6e38ec50039951f13f86525f48f2
Sanjib Adhya <sanjib@zacksresearch.in>
2025-01-06 08:50:19 -0600

    completed todo list

--- miniproject/dist/index.js
+++ miniproject/dist/index.js
@@ -1,6 +1,48 @@
 "use strict";
 const btn = document.getElementById('btn'); //non-null assertion operator -> generally we dont use it in TS though it's a TS operator to assume the element is not a null
 const input = document.getElementById('todoinput');
+const form = document.querySelector("form");
+const list = document.getElementById('todoList');
+const todos = readToDos();
+todos.forEach(createTodo);
+function readToDos() {
+    const todosJSON = localStorage.getItem("todos");
+    if (todosJSON == null)
+        return [];
+    return JSON.parse(todosJSON);
+    //console.log(JSON.parse(todosJSON))
+}
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+function handleSubmit(e) {
+    e.preventDefault();
+    //console.log('SUBMITTED');
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    saveToDos();
+    input.value = '';
+    // console.log(todos);
+}
+function createTodo(todo) {
+    //const newToDoText = input.value;
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener('change', () => {
+        todo.completed = checkbox.checked;
+        saveToDos();
+    });
+    newLi.append(todo.text);
+    newLi.append(checkbox);
+    list.append(newLi);
+}
+form.addEventListener('submit', handleSubmit);
 // btn?.addEventListener('click', () => { //? is optional chaining operator
 //     console.log(btn);
 // });
@@ -11,9 +53,9 @@ const input = document.getElementById('todoinput');
 // const numChars = (word as string).length;
 // console.log(`${numChars} characters`);
 //real world example for type assertion
-btn.addEventListener('click', () => {
-    alert(input.value);
-    input.value = '';
-});
+// btn.addEventListener('click', () => {
+//     alert(input.value);
+//     input.value = '';
+// })
 //alternate syntax
 //(<HTMLInputElement>input).value => don't use them in react
--- miniproject/src/index.ts
+++ miniproject/src/index.ts
@@ -1,6 +1,66 @@
+interface Todo {
+    text: string;
+    completed: boolean;
+}
+
 const btn = document.getElementById('btn')! as HTMLButtonElement; //non-null assertion operator -> generally we dont use it in TS though it's a TS operator to assume the element is not a null
 
 const input = document.getElementById('todoinput')! as HTMLInputElement;
+
+const form = document.querySelector("form")!;
+
+const list = document.getElementById('todoList')!;
+
+const todos: Todo[] = readToDos();
+todos.forEach(createTodo);
+
+function readToDos(): Todo[] {
+    const todosJSON = localStorage.getItem("todos");
+    if (todosJSON == null) return [];
+    return JSON.parse(todosJSON);
+
+    //console.log(JSON.parse(todosJSON))
+}
+
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function handleSubmit(e: SubmitEvent) {
+    e.preventDefault();
+    //console.log('SUBMITTED');
+    const newTodo: Todo = {
+        text: input.value,
+        completed: false,
+    };
+
+    createTodo(newTodo);
+    todos.push(newTodo);
+
+    saveToDos();
+
+    input.value = '';
+
+    // console.log(todos);
+}
+
+function createTodo(todo: Todo) {
+    //const newToDoText = input.value;
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener('change', () => {
+        todo.completed = checkbox.checked;
+        saveToDos();
+    });
+
+    newLi.append(todo.text);
+    newLi.append(checkbox);
+    list.append(newLi);
+}
+
+form.addEventListener('submit', handleSubmit);
 
 // btn?.addEventListener('click', () => { //? is optional chaining operator
 //     console.log(btn);
@@ -18,10 +78,10 @@ const input = document.getElementById('todoinput')! as HTMLInputElement;
 // console.log(`${numChars} characters`);
 
 //real world example for type assertion
-btn.addEventListener('click', () => {
-    alert(input.value);
-    input.value = '';
-})
+// btn.addEventListener('click', () => {
+//     alert(input.value);
+//     input.value = '';
+// })
 
 //alternate syntax
 //(<HTMLInputElement>input).value => don't use them in react
